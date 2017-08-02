9d415cbcf390ef2b4df69ced6a691d6bd510b4fd
User <User>
2017-08-02 17:59:21 +0300

    add arrow functional to owl carousel, add listJS plugin (not worked yet)

--- public/js/js-upload-photos.js
+++ public/js/js-upload-photos.js
@@ -1,14 +1,68 @@
+var addressList
 $(document).ready(function(){
 
-$('.js-upload-photos').one('click', function(e){
-	$(this).parent().find('.owl-carousel').owlCarousel({
-		items:1,
-		lazyLoad:true,
-		loop:true,
-		margin:10
+	$('.js-upload-photos').one('click', function(e){
+		
+		var $this = $(this).parent().find('.owl-carousel')
+		$this.owlCarousel({
+			items:1,
+			lazyLoad:true,
+			loop:true,
+			margin:10
+		})
+
+		$(document.documentElement).keyup(function (ev) { 
+			var owl = $this;
+			console.log(ev)
+			if (ev.keyCode == 37) {
+				console.log(ev.keyCode)
+				owl.trigger('prev.owl')
+			} else if (ev.keyCode == 39) {
+				console.log(ev.keyCode)
+				owl.trigger('next.owl')
+			}
+		})
+
+		$(this).hide()
+		return false
 	})
-	$(this).hide()
-	return false
-})
+
+
+
+
+
+
+
+
+
+
+	// toggle button
+
+
+	var serachController = document.querySelector('.serachController')
+	document.querySelector('.fa-search').parentNode.addEventListener('click', function(){
+		serachController.classList.toggle('toggle')
+	})
+
+
+
+
+
+
+	// search pluggin initialize
+
+
+	addressList = new List('searchContainer', { 
+		valueNames: ['searchLink']
+	});
+
+
+	// var listObj = new List('container', options);
+
+	// $('#search-field').on('keyup', function() {
+	// 	var searchString = $(this).val();
+	// 	listObj.search(searchString);
+	// });
+
 
 })
